84036dfefe423dfc69852c27cc7266f15709fbc7
rahul8382 <arprankmaster2020@gmail.com>
2023-11-28 16:22:20 +0530

    this is my SEcond push

--- githubProject/src/test/java/practice/FirstClass.java
+++ githubProject/src/test/java/practice/FirstClass.java
@@ -5,6 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 
 		System.out.println("this is first class method ");
+		System.out.println("this is secondPUsh class method ");
 	}
 
 }
---
+++ githubProject/src/test/java/practice/SecondClass.java
@@ -0,0 +1,10 @@
+package practice;
+
+public class SecondClass {
+
+	public static void main(String[] args) {
+
+		System.out.println("this is First MEthod of Second Class");
+	}
+
+}
